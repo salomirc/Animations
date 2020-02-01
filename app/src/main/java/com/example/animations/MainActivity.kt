067b7dacbb286600e3b62d75dad15b8ae74b2ab8
salox <ciprian.salomir@gmail.com>
2020-02-02 01:00:20 +0200

    Translate animation stage 2

--- app/src/main/java/com/example/animations/MainActivity.kt
+++ app/src/main/java/com/example/animations/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.animations
 import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private var scale: Float = 0f
+    private var toggle: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +18,19 @@ class MainActivity : AppCompatActivity() {
         scale = resources.displayMetrics.density
 
         ObjectAnimator.ofFloat(droidImageView, "translationY", 100f * scale ).apply {
+            duration = 2000
+            start()
+        }
+
+        myButton.setOnClickListener {
+            toggle = !toggle
+            if (toggle) translateY(myButton, 100f) else translateY(myButton, 0f)
+        }
+
+    }
+
+    private fun translateY(view: View, transY: Float) {
+        ObjectAnimator.ofFloat(view, "translationY", transY * scale ).apply {
             duration = 2000
             start()
         }
