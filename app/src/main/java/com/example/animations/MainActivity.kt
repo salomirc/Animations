9778069b90f3eaf3c9df41c0cc1ace8ac42e3161
salox <ciprian.salomir@gmail.com>
2020-02-01 23:22:05 +0200

    Translate animation.

--- app/src/main/java/com/example/animations/MainActivity.kt
+++ app/src/main/java/com/example/animations/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.animations
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var scale: Float = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        scale = resources.displayMetrics.density
+
+        ObjectAnimator.ofFloat(droidImageView, "translationY", 100f * scale ).apply {
+            duration = 2000
+            start()
+        }
     }
 }
