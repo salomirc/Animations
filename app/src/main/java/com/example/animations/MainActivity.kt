e5ee08c04b5f478fbbc10cfc13c142da38f13a4f
Ciprian Salomir <ciprian.salomir@gmail.com>
2022-09-19 15:36:03 +0300

    Correct use of resources, update 2

--- app/src/main/java/com/example/animations/MainActivity.kt
+++ app/src/main/java/com/example/animations/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         // The translation distance on the X axis expressed in dp
-        private const val TRANSLATION_X_DP = -100f
+        private const val TRANSLATION_X_DP = 100f
         private const val TAG = "Animations"
     }
 
@@ -29,70 +29,99 @@ class MainActivity : AppCompatActivity() {
         return (valueDp * scale + 0.5).toInt()
     }
 
+    private fun convertDpToRawPx(valueDp: Float): Float {
+        val scale = resources.displayMetrics.density
+        Log.d(TAG, "logical density of the screen : scale = $scale")
+        return (valueDp * scale)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //Equivalent code but not best practices because we should use resources on Android
         //Keep this solution for the use case when we can't keep the Dp value as a resource
+
+        // Use this if the View method needs dimension as Int
         val translationDpToPx = convertDpToPx(TRANSLATION_X_DP)
         Log.d(TAG, "translationX translationDpToPx = $translationDpToPx")
-        droidImageView.translationX = translationDpToPx.toFloat()
+        // Use this if the View method needs dimension as Float
+        val translationDpToRawPx = convertDpToRawPx(TRANSLATION_X_DP)
+        Log.d(TAG, "translationX translationDpToRawPx = $translationDpToRawPx")
+        droidImageView.translationX = translationDpToRawPx
+
 
         // Best practices
+
+        // Use this if the View method needs dimension as Int
         val droidTranslationX = resources.getDimensionPixelSize(R.dimen.droid_translationX)
         Log.d(TAG, "translationX getDimensionPixelSize = $droidTranslationX")
-        droidImageView.translationX = droidTranslationX.toFloat()
+        // Use this if the View method needs dimension as Float
+        val droidTranslationRawX = resources.getDimension(R.dimen.droid_translationX)
+        Log.d(TAG, "translationX getDimension = $droidTranslationRawX")
+        droidImageView.translationX = droidTranslationRawX
 
         myButton.setOnClickListener {
             toggle = !toggle
-            if (toggle) translateY(myButton, -100f) else translateY(myButton, 0f)
-            linearLayout.addView(
-                TextView(this).apply {
-                    text = resources.getText(R.string.my_text_view_txt)
-                    setBackgroundColor(Color.GREEN)
-                    setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.my_text_size))
-                    val height = resources.getDimensionPixelSize(R.dimen.dynamic_text_view_height)
-
-                    layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, height).apply {
-//                  layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT).apply {
+            val myButtonTranslationRawY = resources.getDimension(R.dimen.my_button_translationY)
+            val myButtonNoTranslationY = resources.getDimension(R.dimen.dimension_zero_dp)
+            if (toggle) translateY(myButton, myButtonTranslationRawY) else translateY(
+                myButton,
+                myButtonNoTranslationY
+            )
+            val myTextView = TextView(this).apply {
+                text = resources.getText(R.string.my_text_view_txt)
+                setBackgroundColor(Color.GREEN)
+                setTextSize(
+                    TypedValue.COMPLEX_UNIT_PX,
+                    resources.getDimension(R.dimen.my_text_size)
+                )
+                val height = resources.getDimensionPixelSize(R.dimen.dynamic_text_view_height)
+                layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    height
+                ).apply {
+//              layoutParams = LinearLayout.LayoutParams(
+//                  LinearLayout.LayoutParams.MATCH_PARENT,
+//                  LinearLayout.LayoutParams.WRAP_CONTENT
+//              ).apply {
                     setMargins(0, 0, 0, 10)
                     gravity = Gravity.CENTER_HORIZONTAL
                 }
-            }, 0)
+            }
+            linearLayout.addView(myTextView, 0)
         }
 
     }
 
-    override fun onResume() {
-        super.onResume()
+        override fun onResume() {
+            super.onResume()
 
-        val droidTranslationY = resources.getDimensionPixelSize(R.dimen.droid_translationY)
-        val animTrans = ObjectAnimator.ofFloat(droidImageView, "translationY", droidTranslationY.toFloat() ).apply {
-            duration = 2000
-        }
-        val animScaleX = ObjectAnimator.ofFloat(droidImageView, "scaleX", 1f, 2f).apply {
-            duration = 2000
-        }
-        val animScaleY = ObjectAnimator.ofFloat(droidImageView, "scaleY", 1f, 2f).apply {
-            duration = 2000
-        }
-        val animFade = ObjectAnimator.ofFloat(droidImageView, "alpha", 1f, 0f).apply {
-            duration = 2000
+            val droidTranslationY = resources.getDimension(R.dimen.droid_translationY)
+            val animTrans =
+                ObjectAnimator.ofFloat(droidImageView, "translationY", droidTranslationY).apply {
+                    duration = 2000
+                }
+            val animScaleX = ObjectAnimator.ofFloat(droidImageView, "scaleX", 1f, 2f).apply {
+                duration = 2000
+            }
+            val animScaleY = ObjectAnimator.ofFloat(droidImageView, "scaleY", 1f, 2f).apply {
+                duration = 2000
+            }
+            val animFade = ObjectAnimator.ofFloat(droidImageView, "alpha", 1f, 0f).apply {
+                duration = 2000
+            }
+
+            AnimatorSet().apply {
+                play(animTrans).with(animScaleX).with(animScaleY).before(animFade)
+                start()
+            }
         }
 
-        AnimatorSet().apply {
-            play(animTrans).with(animScaleX).with(animScaleY).before(animFade)
-            start()
+        private fun translateY(view: View, transYPx: Float) {
+            ObjectAnimator.ofFloat(view, "translationY", transYPx).apply {
+                duration = 2000
+                start()
+            }
         }
     }
-
-    private fun translateY(view: View, transY: Float) {
-        val transYPx = convertDpToPx(transY)
-        Log.d(TAG, "transYPx from convertDpToPx = $transYPx")
-        ObjectAnimator.ofFloat(view, "translationY", transYPx.toFloat()).apply {
-            duration = 2000
-            start()
-        }
-    }
-}
